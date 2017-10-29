2c569cbddedd0da556fc4f1f7f614bf73566163e
prathap20 <prathap.badavath@gmail.com>
2017-10-29 13:58:10 +0530

    requirment.txt file added

--- mqttsample.py
+++ mqttsample.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import json
 import eventlet
 import requests
@@ -13,8 +14,8 @@ eventlet.monkey_patch()
 app = Flask(__name__)
 app.config['SECRET'] = ''
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-#app.config['MQTT_BROKER_URL'] = 'iot.eclipse.org'
-app.config['MQTT_BROKER_URL'] = '127.0.0.1'
+app.config['MQTT_BROKER_URL'] = 'iot.eclipse.org'
+#app.config['MQTT_BROKER_URL'] = '127.0.0.1'
 app.config['MQTT_BROKER_PORT'] = 1883
 app.config['MQTT_USERNAME'] = ''
 app.config['MQTT_PASSWORD'] = ''
@@ -54,7 +55,8 @@ def send_data():
     value = '{0:02x}'.format(payload_raw).decode('hex').encode('base64')
     print 'VALUE BASE64 :%s' % value
     data = {'dev_id':dev_id,'payload_raw':value}
-    response = requests.post(url='http://172.16.0.154:2021/pushData', json = data)
+    #response = requests.post(url='http://172.16.0.154:2021/pushData', json = data)
+    response = requests.post(url='https://prathap.localtunnel.me/pushData', json = data)
     #response = requests.post(url='ENTER URL', json = data)
     if response.status_code == requests.codes.ok:
         return jsonify(message='sent '+str(value)+' successfully'), 200
@@ -89,5 +91,5 @@ def handle_logging(client,userdata, level, buf):
 
 if __name__ == '__main__':
     #port = int(os.environ.get('PORT', 5000))
-    socketio.run(app,host='0.0.0.0',port=2021)
+    socketio.run(app,host='127.0.0.1', port=2021, use_reloader=True, debug=True)
     #socketio.run(app)
